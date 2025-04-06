cb6bbe93ce0f34be06c8490274bc0c10d45fea30
Jonatha32 <jonaperez038@gmail.com>
2025-04-06 11:50:09 -0300

    fix and task

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      return { status: 200, body: 'Success' };
+      return { status: 200, body: 'success' };
     })
     .catch(() => {
       return new Error();
