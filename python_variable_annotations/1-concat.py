2cb64970984743f2fed3b86fdd54b810400ba316
Jonatha32 <jonaperez038@gmail.com>
2025-04-22 16:30:34 -0300

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function concat that takes a string str1 and a string str2
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """"Returns the concat of two strings."""
+    return str1 + str2
