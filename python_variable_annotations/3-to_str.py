3a89bfe4f80cf937d17027924dada0c1db017b19
Jonatha32 <jonaperez038@gmail.com>
2025-04-23 16:05:44 -0300

    task 3, 4, 5

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function to_str that takes a float n
+"""
+
+
+def to_str(n: float) -> str:
+    """"Returns the string representation of a float"""
+    return str(n)
---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ a type-annotated function sum_list which takes a list input_list
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """"Returns the sum of a list of floats"""
+    return sum(input_list)
