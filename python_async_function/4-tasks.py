bb40e32a67c796db070a8066360abac240b52a53
Jonatha32 <jonaperez038@gmail.com>
2025-05-02 12:25:25 -0300

    task 4

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -13,7 +13,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """"
      The code is nearly identical to wait_n except task_wait_random
     """
-    tasks = [task_wait_n(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 
     for completed in asyncio.as_completed(tasks):
