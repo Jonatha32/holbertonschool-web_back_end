f47d4bee04792679ba0841dc01326bb48004673d
Jonatha32 <jonaperez038@gmail.com>
2025-05-02 12:09:24 -0300

    task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,15 @@
 """
 Write a coroutine called async_generator that takes no arguments.
 """
-
-
 import asyncio
 import random
 from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
+    """
+    a coroutine called async_generator
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
