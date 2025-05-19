bd7a8c22fcbb7891e13a6cab725ab36aae385c21
Jonatha32 <jonaperez038@gmail.com>
2025-05-19 14:03:06 -0300

    task 0 and 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,10 +5,10 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 process.stdin.setEncoding("utf8");
 
 process.stdin.on('data', (data) => {
-    const name = data.trim();
+  const name = data.trim();
     console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  console.log('This important software is now closing');
 });
