f86ffb11336535c8aea4dd71bf39ef510f11b72f
Jonatha32 <jonaperez038@gmail.com>
2025-05-24 14:24:34 -0300

    task 2 3

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,30 @@
-// 2-read_file
-
-const fs = require('fs');
-
-function countStudents(path) {
+// 3-read_file_async
+\\
+async function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, 'utf8');
-
+    const data = await FileSystem.readFile(path, 'utf8');
     const lines = data.split('\n').filter((line) => line.trim() !== '');
-
-    const students = lines.slice(1);
+    const students = line.slice(1);
 
     const fields = {};
+
     students.forEach((student) => {
       const [firstName, , , field] = student.split(',');
+
       if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstName);
     });
-    console.log(`Number of students: ${students.length}`);
 
+    console.log(`Number of students: ${students.length}`);
     for (const [field, names] of Object.entries(fields)) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
+
+    return Promise.resolve();
   } catch (error) {
-    throw new Error('Cannot load the database');
+    return Promise.reject(new Error('Cannot load the database'));
   }
 }
 
