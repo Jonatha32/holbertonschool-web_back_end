b56e066cc2e6f2b9bf6544e2e276c057538c2ff1
Jonatha32 <jonaperez038@gmail.com>
2025-05-24 12:15:25 -0300

    task 0 and 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ process.stdin.setEncoding("utf8");
 
 process.stdin.on('data', (data) => {
   const name = data.trim();
-    console.log(`Your name is: ${name}`);
+    console.log(`Your name is: ${name}\r`);
 });
 
 process.stdin.on('end', () => {
